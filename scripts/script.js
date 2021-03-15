87b3777e4ee64fb573ff312e30f1ac29f864b0cb
mainzonder <no7au53r@gmail.com>
2021-03-15 21:35:31 +0200

    accordion and modal

--- scripts/script.js
+++ scripts/script.js
@@ -45,5 +45,75 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    const accordion = () => {
+        const characteristicsListElem = document.querySelector('.characteristics__list');
+        const characteristicsItemElems = document.querySelectorAll('.characteristics__item');
+
+        characteristicsItemElems.forEach(elem => {
+            if (elem.children[1].classList.contains('active')) {
+              elem.children[1].style.height = `${elem.children[1].scrollHeight}px`;
+    }
+})
+
+        const open = (button, dropDown) => {
+            closeAllDrops(button, dropDown);
+            dropDown.style.height = `${dropDown.scrollHeight}px`;
+            button.classList.add('active');
+            dropDown.classList.add('active');
+        };
+        const close = (button, dropDown) => {
+            button.classList.remove('active');
+            dropDown.classList.remove('active');
+            dropDown.style.height = '';
+        };
+
+        const closeAllDrops = (button, dropDown) => {
+            characteristicsItemElems.forEach((elem) => {
+                if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+                    close(elem.children[0], elem.children[1]);
+                }
+            });
+        };
+
+        characteristicsListElem.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.classList.contains('characteristics__title')) {
+                const parent = target.closest('.characteristics__item');
+                const description = parent.querySelector('.characteristics__description');
+                description.classList.contains('active')
+                    ? close(target, description)
+                    : open(target, description);
+            }
+
+        });
+        document.body.addEventListener('click', (event) => {
+            const target = event.target
+            if (!target.closest('.characteristics__list')) {
+                closeAllDrops();
+            }
+})
+
+        // setTimeout(closeAllDrops, 5000);
+    };
+
+
+    const modal = () => {
+    const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy')
+        const modal = document.querySelector('.modal');
+
+        cardDetailsButtonBuy.addEventListener('click', () => {
+            modal.classList.add('open')
+        })
+        modal.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.classList.contains('modal__close')) {
+
+                modal.classList.remove('open')
+            }
+        })
+    }
+
     tabs();
+    accordion();
+    modal();
 });
